f76e09e1500703bb1e063dd742bc26de1ddcaf8c
mebi36 <chukwudumebinwobi@gmail.com>
2022-10-20 04:14:08 +0100

    Update serializers.py
    added comments to TopItem serializer

--- news/serializers.py
+++ news/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from .models import TopItem
+from .models import TopItem, Comment
 
 
 class TopItemSerializer(serializes.ModelSerializer):
-  """Serializer for local API endpoint for adding news items."""
-  model = TopItem
-  exclude = ['id', 'ext_id']
+    """Serializer for listing news items and adding news items locally."""
+    comments = serializers.SerializerMethodField()
+    class Meta:
+        model = TopItem
+        exclude = ['id', 'ext_id']
+        def get_comments(self, obj):
+            return CommentSerializer(queryset=Comment.objects.filter(content_object=obj)
+                                     
+                                     
+class CommentSerializer(serializers.ModelSerializer):
+    """Serializer for comments."""
+    class Meta:
+        model = Comment
+        fields = ["__all__"]
